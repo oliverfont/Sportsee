12c003322627f59fcea748900c7122da07a846a3
oliverfont <oliverfont@protonmail.com>
2024-04-19 18:38:21 +0200

    w progress on kpi/charts

--- Frontend/src/components/kpiScore.js
+++ Frontend/src/components/kpiScore.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import { ResponsiveContainer, RadialBarChart, RadialBar, Legend } from 'recharts';
+import './styles/kpiScore.css';
 
 const KpiScore = ({ score }) => {
   // Calcul du pourcentage de score (0 à 100)
   const percentage = score * 100;
 
-  // Données pour le graphique
-  const data = [
-    { name: 'Score', score: percentage }
-  ];
-
   return (
-    <ResponsiveContainer width="100%" height={300}>
-      <RadialBarChart innerRadius="10%" outerRadius="80%" data={data} startAngle={180} endAngle={0}>
-        <RadialBar minAngle={15} label={{ fill: '#666', position: 'insideStart' }} background clockWise dataKey="score" />
-        <Legend iconSize={10} width={120} height={140} layout="vertical" verticalAlign="middle" align="right" />
-      </RadialBarChart>
-    </ResponsiveContainer>
+    <div className="kpi-score-container">
+      <div className="kpi-score-circle" style={{ '--percentage': percentage + '%' }}>
+        <span className="kpi-score-text">{percentage}% de votre objectif</span>
+      </div>
+    </div>
   );
 };
 
