81a7a3b568a3836826567a81815a9ed78673cbd8
oliverfont <oliverfont@protonmail.com>
2024-04-22 17:17:31 +0200

    w en cour sur barchart

--- Frontend/src/components/bartChart.js
+++ Frontend/src/components/bartChart.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-
 const Activity = ({ data }) => {
     return (
-      <BarChart width={600} height={300} data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="day" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="kilogram" fill="#8884d8" />
-        <Bar dataKey="calories" fill="#82ca9d" />
-      </BarChart>
+        <BarChart width={600} height={300} data={data}>
+            <XAxis domain={[1, 10]} />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="kilogram" fill="#282D30" />
+            <Bar dataKey="calories" fill="#E60000" />
+        </BarChart>
     );
-  };
-  
-  
+};
+
 export default Activity;
